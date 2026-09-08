32194123a7aecd6d4113276790d7e7bbd60893c2
xdecaro <yvv4jfzkbb@privaterelay.appleid.com>
2026-09-09 00:13:54 +0200

    Normalize Inventory 0.2.0 to lowercase xdecaro identity
    Fresh prerelease baseline using com_xdecaroinventory, pkg_xdecaroinventory, xdecaro\Component\Inventory and #__xdecaroinventory_*.

---
+++ component/admin/src/Extension/InventoryComponent.php
@@ -0,0 +1,2 @@
+<?php
+namespace xdecaro\Component\Inventory\Administrator\Extension; defined('_JEXEC') or die; use Joomla\CMS\Extension\MVCComponent; final class InventoryComponent extends MVCComponent {}
